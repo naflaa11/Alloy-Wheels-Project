1c563d270df51f0d42d5e953df94803d6425de87
naflaa11 <53143354+naflaa11@users.noreply.github.com>
2019-12-03 07:33:41 +0200

    Add files via upload

--- crm_notification/crm_notification/__manifest__.py
+++ crm_notification/crm_notification/__manifest__.py
@@ -9,7 +9,7 @@
         Long description of module's purpose
     """,
 
-    'author': "Telenoc",
+    'author': "Hunain",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
